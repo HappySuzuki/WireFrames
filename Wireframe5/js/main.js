c156b36d13c9d4c7f4982165f5153461787bacdf
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-02-02 12:29:07 -0800

    wf5 refactor

--- Wireframe5/js/main.js
+++ Wireframe5/js/main.js
@@ -1,4 +1,4 @@
-var satelliteArray = [
+var people = [
   {
     name: 'test1',
     age: 13
@@ -13,7 +13,20 @@ var satelliteArray = [
   }
 ];
 
-for (var i = 0; i < satelliteArray.length; i++) {
-  console.log('Name is: ', satelliteArray[i].name);
-  console.log('Age is: ', satelliteArray[i].age);
+let result = document.getElementById('result');
+
+for (var i = 0; i < people.length; i++) {
+  var person = people[i];
+
+  result.innerHTML =
+    result.innerHTML +
+    '<div class="person"> Name is' +
+    '<span class="name">' +
+    person.name +
+    '</span>' +
+    ' and Age is' +
+    '<span class="age">' +
+    person.age +
+    ' </span>' +
+    '</div>';
 }
