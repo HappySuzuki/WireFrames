e0c953bad7ea71f60eae312b6e51f82f6b28863c
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-02-02 11:05:10 -0800

    add js wf5

--- Wireframe5/js/main.js
+++ Wireframe5/js/main.js
@@ -1,1 +1,19 @@
-console.log('main.js');
+var satelliteArray = [
+  {
+    name: 'test1',
+    age: 13
+  },
+  {
+    name: 'test2',
+    age: 14
+  },
+  {
+    name: 'test2',
+    age: 15
+  }
+];
+
+for (var i = 0; i < satelliteArray.length; i++) {
+  console.log('Name is: ', satelliteArray[i].name);
+  console.log('Age is: ', satelliteArray[i].age);
+}
